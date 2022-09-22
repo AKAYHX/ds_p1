c5b5e476e8a2ea4d7ef8fb91e4d386a17f3b3519
lichyyyyyy <lichy@ruc.edu.cn>
2022-09-22 18:04:33 -0400

    complete NewClient function

--- src/github.com/cmu440/lsp/client_impl.go
+++ src/github.com/cmu440/lsp/client_impl.go
@@ -2,10 +2,16 @@
 
 package lsp
 
-import "errors"
+import (
+	"encoding/json"
+	"errors"
+	"github.com/cmu440/lspnet"
+)
 
 type client struct {
 	// TODO: implement this!
+	connID int
+	currentSeqNum int
 }
 
 // NewClient creates, initiates, and returns a new client. This function
@@ -22,7 +28,41 @@ type client struct {
 // hostport is a colon-separated string identifying the server's host address
 // and port number (i.e., "localhost:9999").
 func NewClient(hostport string, initialSeqNum int, params *Params) (Client, error) {
-	return nil, errors.New("not yet implemented")
+	addr, err := lspnet.ResolveUDPAddr("udp", hostport)
+	if err != nil {
+		return nil, err
+	}
+	udpConn, err := lspnet.ListenUDP("udp", addr)
+	if err != nil {
+		return nil, err
+	}
+
+	// Send connect message
+	request, err := json.Marshal(NewConnect(initialSeqNum))
+	if err != nil {
+		return nil, err
+	}
+	_, err = udpConn.WriteToUDP(request, addr)
+	if err != nil {
+		return nil, err
+	}
+
+	var buffer []byte
+	var response Message
+	bytes, _, err := udpConn.ReadFromUDP(buffer)
+	if err = json.Unmarshal(buffer[:bytes], &response); err != nil {
+		return nil, err
+	}
+
+	// Check if the connection is valid
+	if response.Type == MsgAck && response.SeqNum == initialSeqNum {
+		return &client{
+			connID: response.ConnID,
+			currentSeqNum: initialSeqNum,
+		}, nil
+	}
+
+	return nil, errors.New("failed to connect due to invalid response")
 }
 
 func (c *client) ConnID() int {
