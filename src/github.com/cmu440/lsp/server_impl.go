7d0a2e9d6021655bd829099682cdfbc8410f9fdd
akayhx <377152718@qq.com>
2022-10-04 01:21:08 -0400

    finished server sliding window

--- src/github.com/cmu440/lsp/server_impl.go
+++ src/github.com/cmu440/lsp/server_impl.go
@@ -12,31 +12,43 @@ import (
 const Maxsize = 2000
 
 type server struct {
-	conn             *lspnet.UDPConn
-	readChan         chan *Msg
-	writeChan        chan *Msg
-	preWriteChan     chan *writeData
-	outChan          chan *Message
-	connectChan      chan *Msg
-	clients          map[int]*serverClient
-	id               int
-	ackChan          chan *Msg
-	closeMain        chan bool
-	closeRead        chan bool
-	closeWrite       chan bool
-	closeBuffer      chan bool
-	closeHandleWrite chan bool
-	closeConnect     chan bool
-	closeAck         chan bool
-	closeSeqNum      chan bool
-	closeMsg         chan bool
-	bufferChan       chan *serverClient
-	seqAdd           chan *serverClient
-	seqRead          chan *serverClient
-	seqRes           chan int
-	writeMsg         chan *writeClient
-	readMsg          chan *readClient
-	resMsg           chan *Message
+	conn               *lspnet.UDPConn
+	readChan           chan *Msg
+	writeChan          chan *Msg
+	preWriteChan       chan *writeData
+	outChan            chan *Message
+	connectChan        chan *Msg
+	clients            map[int]*serverClient
+	id                 int
+	ackChan            chan *Msg
+	closeMain          chan bool
+	closeRead          chan bool
+	closeWrite         chan bool
+	closeBuffer        chan bool
+	closeConnect       chan bool
+	closeAck           chan bool
+	closeSeqNum        chan bool
+	closeMsg           chan bool
+	bufferChan         chan *serverClient
+	seqAdd             chan *serverClient
+	seqRead            chan *serverClient
+	seqRes             chan int
+	writeMsg           chan *writeClient
+	readMsg            chan *readClient
+	resMsg             chan *Message
+	EpochLimit         int
+	EpochMillis        int
+	WindowSize         int
+	MaxBackOffInterval int
+	MaxUnackedMessages int
+	clientBuffer       chan int
+	closeClientBuff    chan bool
+	serverseqAdd       chan *serverClient
+	serverseqRead      chan *serverClient
+	serverseqRes       chan int
+	clientWinReq       chan *clientWindow
+	clientWinRes       chan int
+	unAck              chan bool
 }
 
 type serverClient struct {
@@ -46,6 +58,9 @@ type serverClient struct {
 	msgList      map[int]*Message
 	closed       bool
 	serverSeqNum int
+	unAcked      []*Message
+	buffer       []*Msg
+	left         int
 }
 
 type Msg struct {
@@ -67,6 +82,11 @@ type writeClient struct {
 	client  *serverClient
 	seq     int
 	message *Message
+}
+
+type clientWindow struct {
+	client *serverClient
+	num    int
 }
 
 // NewServer creates, initiates, and returns a new server. This function should
@@ -102,42 +122,84 @@ func NewServer(port int, params *Params) (Server, error) {
 		make(chan bool),
 		make(chan bool),
 		make(chan bool),
-		make(chan bool),
 		make(chan *serverClient),
 		make(chan *serverClient),
 		make(chan *serverClient),
 		make(chan int),
 		make(chan *writeClient),
 		make(chan *readClient),
-		make(chan *Message)}
+		make(chan *Message),
+		params.EpochLimit,
+		params.EpochMillis,
+		params.WindowSize,
+		params.MaxBackOffInterval,
+		params.MaxUnackedMessages,
+		make(chan int),
+		make(chan bool),
+		make(chan *serverClient),
+		make(chan *serverClient),
+		make(chan int),
+		make(chan *clientWindow),
+		make(chan int),
+		make(chan bool, 1),
+	}
 	go s.mainRoutine()
 	go s.readRoutine()
 	go s.writeRoutine()
 	go s.connectRoutine()
 	go s.ackRoutine()
 	go s.bufferRoutine()
-	go s.handleWrite()
 	go s.seqNumRoutine()
 	go s.msgRoutine()
+	go s.clientBufferRoutine()
+	go s.handleWrite()
+	go s.windowLeft()
 	return s, nil
 }
 
 func (s *server) mainRoutine() {
 	for {
+		//fmt.Println(len(s.readChan), len(s.preWriteChan), len(s.clientBuffer))
 		select {
 		case msg := <-s.readChan:
+			//fmt.Println("readchan")
 			message := msg.message
+			id := msg.message.ConnID
+			client := s.clients[id]
 			switch message.Type {
 			case MsgConnect:
 				s.connectChan <- msg
-			case MsgAck:
-				continue
-			case MsgCAck:
-				continue
+			case MsgAck, MsgCAck:
+				//fmt.Println("2")
+				s.unAck <- true
+				//fmt.Println(len(client.unAcked))
+				for i := 0; i < len(client.unAcked); i++ {
+					//fmt.Println(i)
+					if client.unAcked[i].SeqNum == message.SeqNum {
+						//fmt.Println("!!")
+						client.unAcked = append(client.unAcked[:i], client.unAcked[i+1:]...)
+						//fmt.Println("callbuff")
+						break
+					}
+				}
+				//fmt.Println("x")
+				if len(client.unAcked) == 0 {
+					if len(client.buffer) > 0 {
+						s.clientWinReq <- &clientWindow{client, client.buffer[0].message.SeqNum}
+					} else {
+						s.serverseqRead <- client
+						num := <-s.serverseqRes
+						s.clientWinReq <- &clientWindow{client, num}
+					}
+				} else {
+					s.clientWinReq <- &clientWindow{client, client.unAcked[0].SeqNum}
+				}
+				//fmt.Println("xx")
+				<-s.unAck
+				s.clientBuffer <- id
+				//fmt.Println("222")
 			case MsgData:
 				s.ackChan <- msg
-				id := msg.message.ConnID
-				client := s.clients[id]
 				seq := msg.message.SeqNum
 				s.seqRead <- client
 				currSeq := <-s.seqRes
@@ -153,7 +215,76 @@ func (s *server) mainRoutine() {
 			return
 		}
 	}
-	return
+}
+
+func (s *server) windowLeft() {
+	for {
+		select {
+		case win := <-s.clientWinReq:
+			if win.num == -1 {
+				s.clientWinRes <- win.client.left
+			} else {
+				win.client.left = win.num
+			}
+		}
+	}
+}
+
+func (s *server) handleWrite() {
+	for {
+		select {
+		case writedata := <-s.preWriteChan:
+			connId := writedata.connId
+			payload := writedata.payload
+			client := s.clients[writedata.connId]
+			SeqNum := client.serverSeqNum + 1
+			s.serverseqAdd <- client
+			size := len(writedata.payload)
+			sum := CalculateChecksum(connId, SeqNum, size, payload)
+			message := NewData(connId, SeqNum, size, payload, sum)
+			msg := &Msg{client.addr, message}
+			s.clientWinReq <- &clientWindow{client, -1}
+			left := <-s.clientWinRes
+			//fmt.Println("3")
+			s.unAck <- true
+			//fmt.Println("33")
+			if len(client.unAcked) < s.MaxUnackedMessages && left+s.WindowSize >= SeqNum {
+				//fmt.Println("towrite")
+				client.unAcked = append(client.unAcked, message)
+				s.writeChan <- msg
+				//fmt.Println("wrote")
+			} else {
+				//fmt.Println("tobuff")
+				client.buffer = append(client.buffer, msg)
+			}
+			<-s.unAck
+			//fmt.Println("333")
+		}
+	}
+}
+
+func (s *server) clientBufferRoutine() {
+	for {
+		select {
+		case id := <-s.clientBuffer:
+			client := s.clients[id]
+			s.clientWinReq <- &clientWindow{client, -1}
+			left := <-s.clientWinRes
+			//fmt.Println("4")
+			s.unAck <- true
+			//fmt.Println("44")
+			if len(client.buffer) > 0 && len(client.unAcked) < s.MaxUnackedMessages && left+s.WindowSize > client.buffer[0].message.SeqNum {
+				//fmt.Println("tobuff")
+				s.writeChan <- client.buffer[0]
+				client.buffer = client.buffer[1:]
+				//fmt.Println("buffclear")
+			}
+			<-s.unAck
+			//fmt.Println("444")
+		case <-s.closeClientBuff:
+			return
+		}
+	}
 }
 
 func (s *server) bufferRoutine() {
@@ -204,6 +335,10 @@ func (s *server) seqNumRoutine() {
 			client.SeqNum += 1
 		case client := <-s.seqRead:
 			s.seqRes <- client.SeqNum
+		case client := <-s.serverseqAdd:
+			client.serverSeqNum += 1
+		case client := <-s.serverseqRead:
+			s.serverseqRes <- client.serverSeqNum
 		}
 	}
 }
@@ -233,6 +368,9 @@ func (s *server) connectRoutine() {
 				msg.addr,
 				make(map[int]*Message),
 				false,
+				0,
+				make([]*Message, 0),
+				make([]*Msg, 0),
 				0}
 			ack := NewAck(s.id, msg.message.SeqNum)
 			msg = &Msg{msg.addr, ack}
@@ -258,28 +396,10 @@ func (s *server) readRoutine() {
 				return
 			}
 			msg := &Msg{addr, &message}
+			//fmt.Println(message.Payload)
+			//fmt.Println(len(s.readChan), cap(s.readChan))
 			s.readChan <- msg
-		}
-	}
-	return
-}
-
-func (s *server) handleWrite() {
-	for {
-		select {
-		case writedata := <-s.preWriteChan:
-			connId := writedata.connId
-			payload := writedata.payload
-			client := s.clients[writedata.connId]
-			client.serverSeqNum += 1
-			SeqNum := client.serverSeqNum
-			size := len(writedata.payload)
-			sum := CalculateChecksum(connId, SeqNum, size, payload)
-			message := NewData(connId, SeqNum, size, payload, sum)
-			msg := &Msg{client.addr, message}
-			s.writeChan <- msg
-		case <-s.closeHandleWrite:
-			return
+			//fmt.Println("sent to read")
 		}
 	}
 }
@@ -288,16 +408,16 @@ func (s *server) writeRoutine() {
 	for {
 		select {
 		case msg := <-s.writeChan:
+			//fmt.Println(msg.message.Type)
 			buffer, err := json.Marshal(msg.message)
 			if err != nil {
-				//fmt.Println(err)
+				continue
 			}
 			s.conn.WriteToUDP(buffer, msg.addr)
 		case <-s.closeWrite:
 			return
 		}
 	}
-	return
 }
 
 func (s *server) Read() (int, []byte, error) {
@@ -313,6 +433,7 @@ func (s *server) Write(connId int, payload []byte) error {
 	}
 	data := &writeData{connId, payload}
 	s.preWriteChan <- data
+	//fmt.Println("sent to write")
 	return nil
 }
 
@@ -331,10 +452,10 @@ func (s *server) Close() error {
 	s.closeRead <- true
 	s.closeWrite <- true
 	s.closeBuffer <- true
-	s.closeHandleWrite <- true
 	s.closeConnect <- true
 	s.closeAck <- true
 	s.closeSeqNum <- true
 	s.closeMsg <- true
+	s.closeClientBuff <- true
 	return nil
 }
